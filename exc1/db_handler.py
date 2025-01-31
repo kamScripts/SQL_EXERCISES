a64bd1f06b572fd82225decc1303edc4a52752eb
GeKamB <k.gebski1@gmail.com>
2025-01-31 09:09:45 +0000

    update comments

--- exc1/db_handler.py
+++ exc1/db_handler.py
@@ -1,16 +1,15 @@
 import sqlite3
 
 class Db_handler:
-    
+
     def __init__(self, db_name):
       self._db_name = db_name
 
-      
     @property
     def db_name(self) -> str:
         """Getter for Database name (read-only)"""
         return self._db_name
-    
+
     def create_db(self) -> None:
         """Create the database"""
         connection = sqlite3.connect(self.db_name)
@@ -25,13 +24,15 @@ class Db_handler:
             connection.commit()
     def if_table_exists(self, table_name: str) -> bool:
         """Check if a table exists in the SQLite database."""
-        # sqlite_master is a table that contains records of all tables inside the database.
-        # including type, name,, tbl_name, rootpage, and sql.
-        query = "SELECT name FROM sqlite_master WHERE type='table' AND name=?;"
+        # sqlite_master is a table that contains records of all tables
+        # inside the database. including type, name, tbl_name, rootpage, and sql.
+        query = """SELECT name FROM sqlite_master
+        WHERE type='table' AND name=?;"""
 
         with sqlite3.connect(self.db_name) as connection:
             cursor = connection.cursor()
             result = cursor.execute(query, (table_name,)).fetchone()
-        # if table exists fetchone() returns tuple with table name, otherwise returns NoneType.
+        # if table exists fetchone() returns tuple with table name,
+        # otherwise returns NoneType.
         return result is not None
 
